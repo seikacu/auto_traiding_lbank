796f37853a3a491daff2257d821547474106fea3
seikacu <neoxak@gmail.com>
2023-08-19 15:03:13 +0300

    reformat code

--- main.py
+++ main.py
@@ -20,40 +20,45 @@ def open_browser():
     sys.exit()
     os._exit(0)
 
-def get_link(driver:webdriver.Chrome):
+
+def get_link(driver: webdriver.Chrome):
     url = str(input("Введите ссылку: "))
     # driver.get("https://www.lbank.com/en-US/trade/btc_usdt/")
     driver.get(url)
 
-def thread_by(driver:webdriver.Chrome, dialog_semaphore):
+
+def thread_by(driver: webdriver.Chrome, dialog_semaphore):
     dialog_semaphore.acquire()
     click_order(driver, "Market")
     dialog_semaphore.acquire()
     turn_trade_slider(driver, "tradeSliderGreen")
-    #dialog_semaphore.acquire()
-    #set_amount(driver, "Enter buying amount", "0.01")
+    # dialog_semaphore.acquire()
+    # set_amount(driver, "Enter buying amount", "0.01")
     dialog_semaphore.acquire()
     click_trade_button(driver, "index_buy")
 
-def thread_sell(driver:webdriver.Chrome, dialog_semaphore):
+
+def thread_sell(driver: webdriver.Chrome, dialog_semaphore):
     dialog_semaphore.acquire()
     click_order(driver, "Market")
     dialog_semaphore.acquire()
     turn_trade_slider(driver, "tradeSliderRed")
-    #dialog_semaphore.acquire()
-    #set_amount(driver, "Enter selling amount", "0.01")
+    # dialog_semaphore.acquire()
+    # set_amount(driver, "Enter selling amount", "0.01")
     dialog_semaphore.acquire()
     click_trade_button(driver, "index_sel")
+
 
 def get_driver():
     options = webdriver.ChromeOptions()
     set_driver_options(options)
     caps = DesiredCapabilities().CHROME
     caps['pageLoadStrategy'] = 'normal'
-    
+
     service = Service(desired_capabilities=caps, executable_path=r"C:\WebDriver\chromedriver\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
     return driver
+
 
 def mode_list():
     try:
@@ -71,7 +76,8 @@ def mode_list():
             driver = get_driver()
             stop_threads = False
             # Создание и запуск потока для выполнения проверки на всплывающее диалоговое окно
-            thread = threading.Thread(target=check_dialog_thread, args=(lambda : stop_threads, driver, dialog_semaphore, ))
+            thread = threading.Thread(target=check_dialog_thread,
+                                      args=(lambda: stop_threads, driver, dialog_semaphore,))
             thread.start()
 
         if mode == 0:
@@ -81,11 +87,11 @@ def mode_list():
         if mode == 2:
             get_link(driver)
         if mode == 3:
-            threadBy = threading.Thread(target=thread_by, args=(driver, dialog_semaphore, ))
+            threadBy = threading.Thread(target=thread_by, args=(driver, dialog_semaphore,))
             threadBy.start()
             threadBy.join()
         if mode == 4:
-            threadSell = threading.Thread(target=thread_sell, args=(driver, dialog_semaphore, ))
+            threadSell = threading.Thread(target=thread_sell, args=(driver, dialog_semaphore,))
             threadSell.start()
             threadSell.join()
     except Exception as ex:
@@ -95,16 +101,19 @@ def mode_list():
             stop_threads = True
             thread.join()
 
-def set_driver_options(options:webdriver.ChromeOptions):
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 YaBrowser/23.5.2.625 Yowser/2.5 Safari/537.36")
+
+def set_driver_options(options: webdriver.ChromeOptions):
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 YaBrowser/23.5.2.625 Yowser/2.5 Safari/537.36")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
 
     options.debugger_address = 'localhost:8989'
 
+
 # Функция для проверки наличия класса 'new_class'
-def check_dialog_class(driver :webdriver.Chrome, dialog_semaphore):
+def check_dialog_class(driver: webdriver.Chrome, dialog_semaphore):
     try:
         element = driver.find_element(By.CLASS_NAME, CLASS_NAME)
         # Действия после появления класса class_name
@@ -119,17 +128,19 @@ def check_dialog_class(driver :webdriver.Chrome, dialog_semaphore):
         pass
     finally:
         # Release the semaphore to allow waiting threads to continue
-        dialog_semaphore.release()      
-              
+        dialog_semaphore.release()
+
+
 # Функция для выполнения проверки в отдельном потоке
-def check_dialog_thread(stop, driver:webdriver.Chrome, dialog_semaphore):
+def check_dialog_thread(stop, driver: webdriver.Chrome, dialog_semaphore):
     while True:
         check_dialog_class(driver, dialog_semaphore)
         if stop():
             break
 
+
 # Passing authentication...
-def authentication(driver:webdriver.Chrome):
+def authentication(driver: webdriver.Chrome):
     try:
         driver.get("https://www.lbank.com/login/")
         email_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter your email']")
@@ -139,14 +150,15 @@ def authentication(driver:webdriver.Chrome):
         password_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter password']")
         password_input.clear()
         password_input.send_keys(bank_password)
-        
+
         password_input.send_keys(Keys.ENTER)
     except Exception:
         print("Поля аутентификации не найдены или уже авторизованы")
         pass
 
+
 # нажать Market
-def click_order(driver:webdriver.Chrome, arg:str):
+def click_order(driver: webdriver.Chrome, arg: str):
     try:
         order = driver.find_element(By.XPATH, f"//div[contains(text(),'{arg}')]")
         driver.execute_script("arguments[0].click();", order)
@@ -154,8 +166,9 @@ def click_order(driver:webdriver.Chrome, arg:str):
         print(f"Кнопка {arg} не найдена")
         pass
 
+
 # установить значение amount
-def set_amount(driver:webdriver.Chrome, arg:str, val:str):
+def set_amount(driver: webdriver.Chrome, arg: str, val: str):
     try:
         input = driver.find_element(By.XPATH, f"//input[@placeholder='{arg}']")
         for i in range(6):
@@ -165,8 +178,9 @@ def set_amount(driver:webdriver.Chrome, arg:str, val:str):
         print(f"Поле для ввода {arg} не найдено")
         pass
 
+
 # установка слайдера в максимальное значение
-def turn_trade_slider(driver:webdriver.Chrome, arg:str):
+def turn_trade_slider(driver: webdriver.Chrome, arg: str):
     try:
         slider = driver.find_element(By.XPATH, f"//div[contains(@class, '{arg}')]//span[@style='left: 100%;']")
         slider.click()
@@ -174,8 +188,9 @@ def turn_trade_slider(driver:webdriver.Chrome, arg:str):
         print(f"Не могу сдвинуть слайдер {arg}")
         pass
 
+
 # нажать кнопку buy/sell
-def click_trade_button(driver:webdriver.Chrome, arg:str):
+def click_trade_button(driver: webdriver.Chrome, arg: str):
     try:
         button = driver.find_element(By.XPATH, f"//button[contains(@class, '{arg}')]")
         driver.execute_script("arguments[0].click();", button)
@@ -183,9 +198,10 @@ def click_trade_button(driver:webdriver.Chrome, arg:str):
         print(f"Кнопка {arg} не найдена")
         pass
 
+
 # нажать кнопку cancel/confirm Order
 # передаются два аргумента args {Cancel} / {Confirm}
-def click_trade_confirm_button(driver:webdriver.Chrome, arg:str):
+def click_trade_confirm_button(driver: webdriver.Chrome, arg: str):
     try:
         span = driver.find_element(By.XPATH, f"//span[contains(text(), '{arg}')]")
         button = span.find_element(By.XPATH, "./parent::button")
@@ -194,8 +210,9 @@ def click_trade_confirm_button(driver:webdriver.Chrome, arg:str):
         print(f"Кнопка {arg} не найдена")
         pass
 
+
 # активировать чек-бокс "Don't prompt again"
-def click_dont_prompt_again(driver:webdriver.Chrome):
+def click_dont_prompt_again(driver: webdriver.Chrome):
     try:
         span = driver.find_element(By.XPATH, "//span[contains(text(), 'prompt again')]")
         check = span.find_element(By.XPATH, "./parent::div")
@@ -204,17 +221,20 @@ def click_dont_prompt_again(driver:webdriver.Chrome):
         print("Чек-бокс не найден")
         pass
 
+
 # закрыть всплывающее диалоговое окно
-def close_dialog_window(driver:webdriver.Chrome, dialog):
+def close_dialog_window(driver: webdriver.Chrome, dialog):
     try:
         closeButton = dialog.find_element(By.XPATH, "//button[contains(@aria-label, 'Close')]")
         driver.execute_script("arguments[0].click();", closeButton)
     except NoSuchElementException:
-        print("Кнопка Close не найдена")    
-        pass       
+        print("Кнопка Close не найдена")
+        pass
 
-# Нажать кнопку I see
-def click_i_see(driver:webdriver.Chrome, dialog):
+    # Нажать кнопку I see
+
+
+def click_i_see(driver: webdriver.Chrome, dialog):
     try:
         span = dialog.find_element(By.XPATH, "//span[contains(text(), 'I see')]")
         button = span.find_element(By.XPATH, "./parent::button")
@@ -222,6 +242,7 @@ def click_i_see(driver:webdriver.Chrome, dialog):
     except NoSuchElementException:
         print("Кнопка I see не найдена")
         pass
+
 
 def main():
     print("start")
